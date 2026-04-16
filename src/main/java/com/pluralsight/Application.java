2ea199fea8842320d20a311bcaef681c0a8d2848
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-16 00:18:03 -0500

    feat: Enabled i solution for highest

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
+//Step 1
 public class Application {
+    public static void main(String[] args) {
+        int[] testScores = {100, 28, 13, 51, 73, 81, 86, 91, 63, 0};
+        int sum = 0;
+
+        Arrays.sort(testScores);
+        for (int testScore : testScores) {
+            System.out.println(testScore);
+            sum += testScore;
+        }
+        //Average
+        int average = (int) sum / testScores.length;
+        System.out.println("The average test score for the class is " + average);
+//        High Score
+        int highest = testScores.length - 1;
+        System.out.println("The highest test score for the class is " +testScores[highest]);
+//        Low Score
+        System.out.println("The lowest test score for the class is " + (testScores[0]));
+    }
+//    Median
+
+
+
 }
